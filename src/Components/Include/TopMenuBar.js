0ff11e9bb500d6ed0e7ef3e3a23defa03c9889c4
mahalakshmiyml <lakshmiyml@gmail.com>
2022-01-13 15:22:40 +0530

    hariHara

--- src/Components/Include/TopMenuBar.js
+++ src/Components/Include/TopMenuBar.js
@@ -9,56 +9,59 @@ const TopMenuBar = () => {
         <Container>
           <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Collapse id="responsive-navbar-nav">  
             <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>              
             {/* Property Consultant Dropdown start */}
               <NavDropdown title="Property Consultant" id="collasible-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/proprety-consultants/property-evalution">Property Evaluation</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/taxation">Taxation</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/buy-sell">Buy/Sell</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/home-search">Home Search</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/documentation">Documentation</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/banking-assist">Banking Assist</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/investment">Investment</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/proprety-consultants/project-marketing">Project Marketing</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/proprety-consultants/taxation">Taxation</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/proprety-consultants/buy-sell">Buy/Sell</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/proprety-consultants/home-search">Home Search</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/proprety-consultants/documentation">Documentation</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/proprety-consultants/banking-assist">Banking Assist</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/proprety-consultants/investment">Investment</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/proprety-consultants/project-marketing">Project Marketing</NavDropdown.Item>
+                <NavDropdown.Divider />                
               </NavDropdown>
               {/*Property Consultant Dropdown end*/}
               {/* Property Management Dropdown start */}
               <NavDropdown title="Property Management" id="collasible-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/property-management/painting">Painting</NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/property-management/electrical">
                   Electrical
                 </NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/property-management/plumbing">
                   Plumbing
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+                </NavDropdown>
 {/* Property Mangament Dropdown end */}
 {/*Other services start*/}
               <NavDropdown title="Other Services" id="collasible-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/other-services/relocations">Relocations</NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/other-services/vaastu-consulting">
                   Vaastu Consulting
                 </NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/other-services/digital-marketing">
                   Digital Marketing
                 </NavDropdown.Item>
+                <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/other-services/web-designs">
                   Web Designs                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
               </NavDropdown>
 {/* Other Service end */}
               <Nav.Link as={Link} to="#blog">Blog</Nav.Link>
